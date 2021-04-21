9e263ad8f4765dbad28cbee7c42f430ad97e3081
abhiparashar <aparashar103@gmail.com>
2021-04-21 10:15:59 +0530

    active class and dropdown implementation

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React,{useState} from "react";
 
 const Accordian = (props) => {
-  const renderlist = props.items.map((item) => {
-    return(
-        <div>
-            {item.title}
-            <div>{item.content}</div>
+  const [activeindex, setActiveindex] = useState(null)
+  const ontitleClick = (index)=>{
+    setActiveindex(index)
+  }
+  const renderlist = props.items.map((item,index) => {
+    const active = activeindex===index ? 'active' : ""
+    return (
+      <React.Fragment key={item.title}>
+        <div className={`title ${active}`} onClick={()=>ontitleClick(index)}>
+          <i className="dropdown icon" />
+          {item.title}
         </div>
-    )
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
   });
-  return <div>{renderlist}</div>;
+  return <div className="ui styled accordion">
+    {renderlist}
+    </div>;
 };
 
 export default Accordian;
